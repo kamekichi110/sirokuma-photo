9298864a81412ea7cb7fca55042a8030652cf279
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2024-01-28 12:10:42 +0900

    set

---
+++ script/loader.js
@@ -0,0 +1,20 @@
+//変数
+const LoaderScreen = document.getElementById('loader-screen');
+
+//ロード完了
+window.onload = setTimeout(() => {
+    loaded();
+}, 200);
+
+//ロード完了時ロード用divを非表示にする
+function loaded() {
+    console.log('Data loading is complete!');
+    console.log('データロードが完了しました！');
+    LoaderScreen.style.display = "none";
+}
+function reload() {
+    LoaderScreen.style.display = "block";
+    setTimeout(() => {
+        start();
+    }, 2100);
+}

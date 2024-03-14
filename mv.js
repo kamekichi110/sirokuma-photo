78a1bca6144de9ea7cb5143ab566442109bcc601
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2024-03-14 11:16:44 +0900

    set

---
+++ mv.js
@@ -0,0 +1,14 @@
+const MV_list = {
+    "data": {
+        "title": "HOME",
+        "url": "https://youtube.com/@kame_ta"
+    }
+};
+const displayElement = document.getElementById('mvList');
+function loaded() {
+    displayElement.innerHTML = "";
+    for (var i = 0; i < Object.keys(MV_list['data'].length); i++) {
+        displayElement.innerHTML += "<h4>" + MV_list[i].title + "</h4><p><a href='" + MV_list[i].url + "'>view</a></p><br><br><hr>"
+    }
+
+}
